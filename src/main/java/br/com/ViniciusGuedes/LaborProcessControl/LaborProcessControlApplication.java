e76000ae257df653c46d75284cf90685d312c3f0
Vinicius0422 <guedescosta123@hotmail.com>
2023-11-18 17:21:41 -0300

    First commit

---
+++ src/main/java/br/com/ViniciusGuedes/LaborProcessControl/LaborProcessControlApplication.java
@@ -0,0 +1,13 @@
+package br.com.ViniciusGuedes.LaborProcessControl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaborProcessControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaborProcessControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ViniciusGuedes/LaborProcessControl/LaborProcessControlApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ViniciusGuedes.LaborProcessControl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaborProcessControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
