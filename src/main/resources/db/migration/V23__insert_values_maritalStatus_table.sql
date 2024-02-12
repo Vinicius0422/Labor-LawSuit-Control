4319d638c26eeec9bd300acd7578b2ea9839adf0
Vinicius0422 <guedescosta123@hotmail.com>
2024-02-12 14:04:41 -0300

    Refactoring dependency injections

---
+++ src/main/resources/db/migration/V23__insert_values_maritalStatus_table.sql
@@ -0,0 +1,6 @@
+INSERT INTO marital_status (marital_status, created_at) VALUES
+    ('Single', '2024-01-15 22:30:00'),
+    ('Married', '2024-01-15 22:30:00'),
+    ('Divorced', '2024-01-15 22:30:00'),
+    ('Widowed', '2024-01-15 22:30:00'),
+    ('Separated', '2024-01-15 22:30:00');
---
+++ src/main/resources/db/migration/V24__insert_values_accountType_table.sql
@@ -0,0 +1,3 @@
+INSERT INTO account_type (account_type, created_at) VALUES
+    ('Savings', '2024-01-15 22:30:00'),
+    ('Checking', '2024-01-15 22:30:00');
